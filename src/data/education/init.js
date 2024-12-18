4a590eac18eb27969b4946b1f6d61b909620582d
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-12-18 01:54:10 +0000

    added education details

---
+++ src/data/education/init.js
@@ -0,0 +1,14 @@
+const education = [
+  {
+    title: "King's College, University of Cambridge",
+    date: "October 2022 - Present",
+    descriptionHTMLFile: "education/university.html",
+  },
+  {
+    title: "King Edward's School, Birmingham",
+    date: "September 2015 - July 2022",
+    descriptionHTMLFile: "education/school.html",
+  },
+];
+
+export default education;
