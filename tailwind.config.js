77ffde5ca5bdd6883346d2b7834b4ca9123d8300
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-12-18 01:44:02 +0000

    fixed Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./js/**/*.js"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    plugins: [],
   },
-  plugins: [],
 };
