edbcf264c5e9b911ff16713bdd484e49e8af7374
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-12-18 01:54:49 +0000

    added projects details

---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    title: "Fictional Airline Website",
+    date: "2013",
+    description:
+      "I created a website for my own fictional airline at the age of 9 in HTML and CSS, using server-side PHP and SQL for login functionality.",
+  },
+  {
+    title: "Global Christian Conference App",
+    date: "2015",
+    description:
+      "I created a mobile app for a global Christian conference at age 11, and maintained it for multiple years with several hundred downloads.",
+  },
+  {
+    title: "Further Web Development and Maintenance",
+    date: "2015-2022",
+    description:
+      "I built and maintained the websites for my local GP surgery, church, and the aforementioned conference, and maintained them for several years.",
+  },
+  {
+    title: "Smart Home System",
+    date: "2020-Present",
+    description:
+      "I built and maintained a smart home system using ESP8266 microchips and Python, allowing for voice-/phone-controlled lighting, heating, and gate/garage doors.",
+  },
+  {
+    title: "Smart Home Control Panels",
+    date: "2020-2021",
+    description:
+      "I created custom smart home control panels using old Android tablets and Kindle Paperwhites. I built them in pure HTML, CSS, and JavaScript.",
+  },
+  {
+    title: "Bare-metal GUI",
+    date: "2020-2021",
+    description:
+      "I built a custom bare-metal GUI in x86 Assembly over lockdown.",
+  },
+  {
+    title: "Social Media App",
+    date: "2021",
+    description:
+      "I developed a mobile social media-style app in React Native with peers, after the conclusion of our joint InvestIN summer internship.",
+  },
+];
+
+export default projects;
