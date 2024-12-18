acd061e6c6397a21ebebf8abd1f26033f92e207e
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-12-18 01:54:29 +0000

    added experience details

---
+++ src/data/experience/init.js
@@ -0,0 +1,37 @@
+const experience = [
+  {
+    title: "Ambassador | OXEX",
+    date: "September 2024 - Present",
+    description:
+      "Following on from our successful collaboration during the King's Affair 2024, I was invited by OXEX to act as an ambassador for their ticketing platform at the University of Cambridge. This role involves promoting the platform to other student societies and event organisers, as well as providing feedback and suggestions for future development.",
+  },
+  {
+    title: "Technology Risk Intern | Ernst & Young (EY)",
+    date: "July 2024",
+    description:
+      "I supported the assessment of IT risks and vulnerabilities at various S&P 400 and S&P 500 firms. Specifically, I helped evaluate IT controls in place as part of financial audits at said firms to comply with IFRS and US GAAP regulations. On top of this, I also conducted research on the risks of AI and other emerging technologies in relation to client auditing services.",
+  },
+  {
+    title: "Ticketing and IT Lead | The King's Affair",
+    date: "February 2023 - June 2024",
+    descriptionHTMLFile: "experience/ka.html",
+  },
+  {
+    title: "Summer Research Intern | University of Cambridge",
+    date: "June 2023 - August 2023",
+    descriptionHTMLFile: "experience/srp.html",
+  },
+  {
+    title: "Customer Assistant | Tesco",
+    date: "July 2022 - September 2022",
+    description:
+      "I worked as a customer assistant at my local Tesco Extra store, picking and fulfilling online orders as a Personal Shopper and assisting customers with their shopping. I developed communication and teamwork skills through working with colleagues and customers.",
+  },
+  {
+    title: "Summer Intern | InvestIN",
+    date: "July 2021 - August 2021",
+    descriptionHTMLFile: "experience/investin.html",
+  },
+];
+
+export default experience;
