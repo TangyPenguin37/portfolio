976d243b05ccbb35769d4f6fe8b74e07841691f1
Samuel Mashil <69389564+TangyPenguin37@users.noreply.github.com>
2024-12-18 01:54:38 +0000

    added skills details

---
+++ src/data/skills.js
@@ -0,0 +1,23 @@
+const skills = [
+  {
+    title: "Programming languages",
+    date: "",
+    description:
+      "Python, JavaScript, Java, C, C++, x86 Assembly, OCaml, SQL, Prolog",
+  },
+  {
+    title: "Hardware",
+    date: "",
+    description:
+      "I designed, built, and programmed circuits with ESP8266 microchips for home automation.",
+  },
+  // {
+  //   title: "Web Development",
+  //   date: "",
+  //   description: [
+  //     "I redesigned and maintained websites for my local church and GP surgery.",
+  //   ],
+  // },
+];
+
+export default skills;
